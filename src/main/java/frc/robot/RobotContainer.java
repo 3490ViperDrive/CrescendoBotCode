8bb1abbc4129065f577441225e61c46578e2da4e
kjr-code <k.rheu.code@gmail.com>
2024-02-03 13:57:00 -0500

    Added Adam's code to main branch

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,10 +4,11 @@
 
 package frc.robot;
 
-//import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
-//import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import frc.robot.subsystems.Drivetrain;
+import frc.robot.subsystems.SpinNEOS;
+import monologue.Logged;
 
 public class RobotContainer {
   public RobotContainer() {
