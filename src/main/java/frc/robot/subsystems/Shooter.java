e51a5ba48dad26eb3c819c42164e82d127163e6c
Adamya <adamyapathak12@gmail.com>
2024-02-10 13:01:16 -0500

    shootCommand();

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -3,15 +3,20 @@ package frc.robot.subsystems;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
+import com.ctre.phoenix6.hardware.TalonFX;
+
 public class Shooter extends SubsystemBase {
     
     public Command shoot(){
-        // The command tha runs motors and shoots the note.
+        // The command that runs motors and shoots the note.
 
         // Detect if the note is in or not (beam breaker)
+
+        TalonFX shooterTalon = new TalonFX(0);
 
         System.out.print("Return something");
 
         return null;
     }
+
 }
