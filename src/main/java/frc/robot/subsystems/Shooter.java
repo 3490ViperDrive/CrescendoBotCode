23384311f99f2163831a4239b14ba5cc5b506b3f
Adamya <adamyapathak12@gmail.com>
2024-02-12 17:52:45 -0500

    shooterCode();

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -1,22 +1,40 @@
+// IDK WHAT I JUST COOOKED BUT I DID COOKED SOMETHING
+// IDK WHAT THIS CODE DOES 
+// THIS ENTIRE CODE MIGHT BE WRONG AND IF IT IS, I WILL FIX IT! 
+
 package frc.robot.subsystems;
 
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.wpilibj.Preferences;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 import com.ctre.phoenix6.hardware.TalonFX;
 
 public class Shooter extends SubsystemBase {
+
+    int shooterMotorID = 0;
+
+    TalonFX shooterMotor; 
     
-    public Command shoot(){
-        // The command that runs motors and shoots the note.
-
-        // Detect if the note is in or not (beam breaker)
-
-        TalonFX shooterTalon = new TalonFX(0);
-
-        System.out.print("Return something");
-
-        return null;
+    public Shooter(){
+        shooterMotor = new TalonFX(shooterMotorID);
+        Preferences.initDouble("shooter motor speed [-1, 1]", 0);
     }
 
+    @Override
+    public void periodic() {};
+
+    public Command shoot() {
+        return run(() -> {
+            shooterMotor.set(MathUtil.clamp(Preferences.getDouble("shooter motor speed [-1, 1]", 0), 0, 0));
+        });
+    }
+
+    public Command stopMotorCommand() {
+        return run(() -> {
+            shooterMotor.stopMotor();
+            shooterMotor.set(0);
+        });
+    }
 }
