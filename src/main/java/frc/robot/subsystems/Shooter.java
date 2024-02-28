767684a9bd6979ad208dffb43834b61dea9fd9f6
Adamya <adamyapathak12@gmail.com>
2024-02-28 16:56:25 -0500

    Test commit

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -29,6 +29,8 @@ public class Shooter extends SubsystemBase {
         });
     }
 
+    // This could go in it's separate file if this is wrong
+
     public static class Pivot extends SubsystemBase {
 
     TalonFX pivotMotor;
@@ -54,7 +56,7 @@ public class Shooter extends SubsystemBase {
     }
 }
 
-// I was just wondering if I could put this here; this could go in it's own class if this is wrong
+// I was just wondering if I could put this here; this could go in it's separate file if this is wrong
 
 public static class ShooterExtension extends SubsystemBase {
 
