fad63968dc4c4a8eb9994155d03a8ef17cc5adfb
kjr-code <k.rheu.code@gmail.com>
2024-03-05 19:05:30 -0500

    Joystick Code Initial Test

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,6 +4,8 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj2.command.button.CommandGenericHID;
+import edu.wpi.first.wpilibj2.command.button.CommandJoystick;
 // import edu.wpi.first.wpilibj2.command.Command;
 // import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
@@ -11,14 +13,15 @@ import edu.wpi.first.wpilibj2.command.*;
 import frc.robot.subsystems.*;
 import frc.robot.subsystems.Shooter.Pivot;
 import frc.robot.subsystems.Shooter.ShooterExtension;
-// import frc.robot.subsystems.SpinNEOS;
-// import monologue.Logged;
 import frc.robot.subsystems.vision.Optometrist;
 import monologue.Logged;
 import static frc.robot.Constants.ControllerConstants.*;
 
+import java.util.*;
+
 public class RobotContainer implements Logged {
   CommandXboxController m_driverController = new CommandXboxController(DriverXbox.kControllerID);
+  CommandJoystick m_driverJoystick = new CommandJoystick(0);
   
   Drivetrain m_drivetrain = new Drivetrain();
 
@@ -36,28 +39,12 @@ public class RobotContainer implements Logged {
 
   public RobotContainer() {
 
-    m_drivetrain.setDefaultCommand(
-      m_drivetrain.driveTeleopCommand(
-        m_driverController::getLeftY,
-        m_driverController::getLeftX,
-        m_driverController::getRightX,
-        m_driverController::getLeftTriggerAxis,
-        m_driverController.getHID()::getAButton,
-        m_driverController.getHID()::getBButton,
-        m_driverController.getHID()::getXButton,
-        m_driverController.getHID()::getYButton,
-        () -> m_driverController.getRightTriggerAxis() > DriverXbox.kThumbstickDeadband) //this is evil but i can't think of a better way of doing it
-    );
-    
-    // m_drivetrain.setDefaultCommand(m_drivetrain.sysIDTranslationCommand(6));
+    //TODO add some way for the user to pick between controller types 
+    //cough cough sendable chooser/dropdown/what have you
 
-    //m_shooter.setDefaultCommand(m_shooter.stopMotorCommand());
-
-    //m_intake.setDefaultCommand(m_intake.stopMotorCommand());
-
-    //m_climb.setDefaultCommand(m_climb.stopMotorCommand());
-
-
+    int choice = 0;
+    //TODO "choice" will be determined by user input
+    setDriveDefault(m_driverController, choice);
     configureBindings();
   }
 
@@ -84,5 +71,50 @@ public class RobotContainer implements Logged {
 
   public Command getAutonomousCommand() {
     return Commands.print("No autonomous command(s) configured");
+  }
+
+  // public void setDriveDefault(CommandXboxController m_driverController, Drivetrain m_drivetrain){
+  //   m_drivetrain.setDefaultCommand(
+  //     m_drivetrain.driveTeleopCommand(
+  //       m_driverController::getLeftY,
+  //       m_driverController::getLeftX,
+  //       m_driverController::getRightX,
+  //       m_driverController::getLeftTriggerAxis,
+  //       m_driverController.getHID()::getAButton,
+  //       m_driverController.getHID()::getBButton,
+  //       m_driverController.getHID()::getXButton,
+  //       m_driverController.getHID()::getYButton,
+  //       () -> m_driverController.getRightTriggerAxis() > DriverXbox.kThumbstickDeadband)
+  //   );
+  // }
+
+  public void setDriveDefault(CommandGenericHID m_driverJoystick, int whichType){
+    switch(whichType){
+      case 0:
+          m_drivetrain.setDefaultCommand(
+          m_drivetrain.driveTeleopCommandGeneric(
+            ()-> m_driverJoystick.getRawAxis(0),
+            ()-> m_driverJoystick.getRawAxis(1),
+            ()->m_driverJoystick.getRawAxis(3))
+          );
+        break;
+      case 1:
+          m_drivetrain.setDefaultCommand(
+          m_drivetrain.driveTeleopCommand(
+          m_driverController::getLeftY,
+          m_driverController::getLeftX,
+          m_driverController::getRightX,
+          m_driverController::getLeftTriggerAxis,
+          m_driverController.getHID()::getAButton,
+          m_driverController.getHID()::getBButton,
+          m_driverController.getHID()::getXButton,
+          m_driverController.getHID()::getYButton,
+          () -> m_driverController.getRightTriggerAxis() > DriverXbox.kThumbstickDeadband)
+          );
+        break;
+      default:
+        break;
+    }
+    
   }
 }
