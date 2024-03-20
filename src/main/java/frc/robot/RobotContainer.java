63ccaffacc0c1e59ba64dd616e80a20ba57a40fb
adam M <adamd10bv@gmail.com>
2024-03-20 17:16:38 -0400

    Clean up RobotContainer.java

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,33 +4,20 @@
 
 package frc.robot;
 
-
 import edu.wpi.first.wpilibj2.command.button.CommandGenericHID;
 import edu.wpi.first.wpilibj2.command.button.CommandJoystick;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
-import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.*;
 import frc.robot.Constants.ControllerConstants.DriverXbox;
-import frc.robot.Constants.LiftPivotSetpoint;
 import frc.robot.subsystems.*;
-import frc.robot.subsystems.vision.BreakTheBeam;
-import frc.robot.subsystems.vision.Optometrist;
 import frc.robot.utils.CommandContainer;
 import monologue.Logged;
-import static frc.robot.Constants.ControllerConstants.*;
 import static frc.robot.Constants.ShooterConstants.*;
 
-import java.io.FileNotFoundException;
-
-import com.pathplanner.lib.auto.AutoBuilder;
 import com.pathplanner.lib.auto.NamedCommands;
 import com.pathplanner.lib.commands.PathPlannerAuto;
-
-
-
-
 
 public class RobotContainer implements Logged {
   CommandXboxController m_driverController = new CommandXboxController(DriverXbox.kControllerID);
@@ -43,52 +30,28 @@ public class RobotContainer implements Logged {
   Climber m_climber = new Climber();
   TrapLift m_lift = new TrapLift();
 
-  SendableChooser<Command> m_Chooser = new SendableChooser<>();
+  SendableChooser<Command> m_chooser = new SendableChooser<>();
    
   CommandContainer m_commandContainer = new CommandContainer(m_intake, m_pivot, m_shooter, m_climber, m_lift);
 
   public RobotContainer() {
-
-    //autoChooser = AutoBuilder.buildAutoChooser()
-    
-  m_Chooser.setDefaultOption("Adam's Auto",new PathPlannerAuto("simpleCenter"));
-  m_Chooser.addOption("2 note left Auto", new PathPlannerAuto("2noteLeftAuto"));
-  m_Chooser.addOption("2 note right Auto", new PathPlannerAuto("2noteRightAuto"));
-  m_Chooser.addOption("3 note middle centerline auto", new PathPlannerAuto("middleCenterlineAuto"));
-  m_Chooser.addOption("3 note left centerline auto", new PathPlannerAuto("leftCenterlineAuto"));
-  m_Chooser.addOption("3 note right centerline auto", new PathPlannerAuto("rightCenterlineAuto"));
-  m_Chooser.addOption("4 note middle Auto", new PathPlannerAuto("middleAutoBasic"));
-  m_Chooser.addOption("4 note left Auto", new PathPlannerAuto("AmpAutoBasic"));
-  m_Chooser.addOption("4 note right auto", new PathPlannerAuto("sourceAutoBasic"));
-  SmartDashboard.putData("THE AutoChoices",m_Chooser);
-    // m_drivetrain.setDefaultCommand(
-    //   m_drivetrain.driveTeleopCommand(
-    //     m_driverController::getLeftY,
-    //     m_driverController::getLeftX,
-    //     m_driverController::getRightX,
-    //     m_driverController::getRightTriggerAxis,
-    //     // m_driverController.getHID()::getAButton,
-    //     // m_driverController.getHID()::getBButton,
-    //     // m_driverController.getHID()::getXButton,
-    //     // m_driverController.getHID()::getYButton,
-    //     () -> false,
-    //     () -> false,
-    //     () -> false,
-    //     () -> false,
-    //     () -> m_driverController.getLeftTriggerAxis() > DriverXbox.kThumbstickDeadband)
-    // ); //this is evil but i can't think of a better way of doing it
-
+    m_chooser.setDefaultOption("Adam's Auto",new PathPlannerAuto("simpleCenter"));
+    m_chooser.addOption("2 note left Auto", new PathPlannerAuto("2noteLeftAuto"));
+    m_chooser.addOption("2 note right Auto", new PathPlannerAuto("2noteRightAuto"));
+    m_chooser.addOption("3 note middle centerline auto", new PathPlannerAuto("middleCenterlineAuto"));
+    m_chooser.addOption("3 note left centerline auto", new PathPlannerAuto("leftCenterlineAuto"));
+    m_chooser.addOption("3 note right centerline auto", new PathPlannerAuto("rightCenterlineAuto"));
+    m_chooser.addOption("4 note middle Auto", new PathPlannerAuto("middleAutoBasic"));
+    m_chooser.addOption("4 note left Auto", new PathPlannerAuto("AmpAutoBasic"));
+    m_chooser.addOption("4 note right auto", new PathPlannerAuto("sourceAutoBasic"));
+    m_chooser.addOption("No Auto", new PrintCommand("No auto was selected. Why would you do this?"));
+    SmartDashboard.putData("THE AutoChoices", m_chooser);
     
     //Temp
     // m_pivot.setDefaultCommand(
     //   m_pivot.runOpenLoop(
     //     m_operatorController::getLeftY, m_operatorController::getRightY));
     // m_drivetrain.setDefaultCommand(m_drivetrain.sysIDTranslationCommand(6));
-
-
-
-
-
 
     NamedCommands.registerCommand("Shooter", m_shooter.shoot(kShooterSpeed, m_intake));
     NamedCommands.registerCommand("Intake", m_intake.takeIn(1));
@@ -98,9 +61,6 @@ public class RobotContainer implements Logged {
     m_pivot.setDefaultCommand(m_pivot.requestPosition(55));
     m_lift.setDefaultCommand(m_lift.idle());
 
-
-
-
     //TODO get "choice" from smartdash/shuffleboard
     String temp = "Joystick";
 
@@ -109,8 +69,6 @@ public class RobotContainer implements Logged {
   }
 
   private void configureBindings() {
-
-
     m_driverJoystick.button(1).whileTrue(m_intake.takeInFancy());
     m_driverJoystick.button(2).whileTrue(m_commandContainer.shootFancy(0.6125)); //Shoot regular;
     m_driverJoystick.button(5).whileTrue(m_commandContainer.retractIntakeFancy());
@@ -118,12 +76,12 @@ public class RobotContainer implements Logged {
     //TODO make button 8 "crawl" (button press)
     //TODO robot oriented toggle on 12
     m_driverJoystick.button(3).onTrue(m_commandContainer.ampHandoffScore()); //Score Amp
-    m_driverJoystick.button(9).whileTrue(m_climber.climb(0.75)); //TODO "lift up"
-    m_driverJoystick.button(11).whileTrue(m_climber.climb(-0.75)); //TODO "lift down"
+    m_driverJoystick.button(9).whileTrue(m_climber.climb(0.75));
+    m_driverJoystick.button(11).whileTrue(m_climber.climb(-0.75));
     m_driverJoystick.button(10).toggleOnTrue(m_commandContainer.raisePivotLiftForClimb());
     m_driverJoystick.button(12).onTrue(m_drivetrain.zeroYawCommand());
     
-     m_driverController.start().onTrue(m_drivetrain.zeroYawCommand());
+    //m_driverController.start().onTrue(m_drivetrain.zeroYawCommand());
 
     //Temp
     //m_operatorController.a().whileTrue(m_pivot.setPosition(LiftPivotSetpoint.kShoot));
@@ -136,11 +94,10 @@ public class RobotContainer implements Logged {
     //m_driverController.b().whileTrue(m_intake.takeInFancy());
     //m_driverController.y().whileTrue(m_intake.takeOut());
 
-    
   }
   
   public Command getAutonomousCommand() {
-    return m_Chooser.getSelected();
+    return m_chooser.getSelected();
     //return Commands.print("No autonomous command(s) configured");
     //return m_commandContainer.shootFancy(1).withTimeout(3); //THIS SIMPLE AUTO BYPASSES THE SENDABLECHOOSER
     //return new PathPlannerAuto("simpleCenter"); //This auto is tested and working
@@ -176,6 +133,4 @@ public class RobotContainer implements Logged {
         break;
     }
   }
-
-  
 }
